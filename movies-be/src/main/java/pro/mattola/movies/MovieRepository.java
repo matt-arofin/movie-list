bb47eecd97b4b349b8064c35d9ccfc4df979b711
Matthew Arofin <Matt.arofin@gmail.com>
2023-01-24 19:41:27 +0100

    MovieController and ID search feature implemented. Moving to ReviewController next

--- movies-be/src/main/java/pro/mattola/movies/MovieRepository.java
+++ movies-be/src/main/java/pro/mattola/movies/MovieRepository.java
@@ -1,4 +1,12 @@
 package pro.mattola.movies;
 
-public interface MovieRepository {
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+  Optional<Movie> findMovieByImdbId(String ImdbId);
 }
